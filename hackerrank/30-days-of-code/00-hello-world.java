6b9bc7321c9c838756204ef740084eeed88d9cb9
Brandon Hill <86802998+everhill@users.noreply.github.com>
2021-07-03 01:06:11 -0500

    Update formatting in Day 0

--- hackerrank/30-days-of-code/00-hello-world.java
+++ hackerrank/30-days-of-code/00-hello-world.java
@@ -16,7 +16,8 @@ public class Solution {
     // Create a Scanner object to read input from stdin.
     Scanner scan = new Scanner(System.in);
 
-    // Read a full line of input from stdin and save it to our variable, inputString.
+    // Read a full line of input from stdin and save it to our variable,
+    // inputString.
     String inputString = scan.nextLine();
 
     // Close the scanner object, because we've finished reading
@@ -26,7 +27,8 @@ public class Solution {
     // Print a string literal saying "Hello, World." to stdout.
     System.out.println("Hello, World.");
 
-    // TODO: Write a line of code here that prints the contents of inputString to stdout.
+    // TODO: Write a line of code here that prints the contents of
+    // inputString to stdout.
     System.out.println(inputString);
   }
 }
